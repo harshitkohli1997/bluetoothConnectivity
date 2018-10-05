f39ef74ea8acf3bae4916dbd14d9d465c2e2d532
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-10-06 04:06:11 +0530

    prototype done

--- app.js
+++ app.js
@@ -3,3 +3,53 @@ const bluetooth = require('node-bluetooth');
 const device = new bluetooth.DeviceINQ();
 
 device.listPairedDevices(console.log);
+
+// device
+// .on('finished',  console.log.bind(console, 'finished'))
+// .on('found', function found(address, name){
+//     if(name ==='')
+//     {
+//   console.log('Found: ' + address + ' with name ' + name + 'present');
+//     }
+    
+// }).scan();
+
+
+
+  
+  // make bluetooth connect to remote device
+
+
+
+
+    
+  device
+  .on('finished', console.log.bind(console, 'finished'))
+  .on('found', function found(address, name){
+  
+    console.log('Found: ' + address + ' with name ' + name);
+  
+    // find serial port channel
+    device.findSerialPortChannel(address, function(channel){
+      console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
+  
+      // make bluetooth connect to remote device
+      bluetooth.connect(address, channel, function(err, connection){
+        if(err) return console.error(err);
+  
+        connection.delimiter = Buffer.from('\n', 'utf8');
+        connection.on('data', (buffer) => {
+          console.log('received message:', buffer.toString());
+        });
+  
+        connection.write(new Buffer('Hello!', 'utf-8'), () => {
+          console.log('wrote');
+        });
+      });
+  
+    });
+  
+  })
+  
+  device.scan();
+    
